944f1b52ca13c355439e11c313cc793db07f823d
epidrome <github@epidro.me>
2018-04-29 19:55:55 +0300

    updated

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,1 +1,31 @@
-var store = []
+var store = [{
+        "title": "Talk_invitation",
+        "excerpt":"Invited for a talk! :sparkles: :smile: ","categories": [],
+        "tags": [],
+        "url": "http://scholar.epidro.me/news/talk_invitation/",
+        "teaser":null},{
+        "title": "Icml_paper_accepted",
+        "excerpt":"Paper accepted! :sparkles: :smile: ","categories": [],
+        "tags": [],
+        "url": "http://scholar.epidro.me/news/icml_paper_accepted/",
+        "teaser":null},{
+        "title": "Project_proposal_accepted",
+        "excerpt":"Proposal accepted! :sparkles: :smile: ","categories": [],
+        "tags": [],
+        "url": "http://scholar.epidro.me/news/project_proposal_accepted/",
+        "teaser":null},{
+        "title": "Welcome!",
+        "excerpt":"Recent papers Garneli, V. and Chorianopoulos, K. 2018. Programming video games and simulations in science education: exploring computational thinking through code analysis. Interactive Learning Environments 26, 3, 386–401. Various aspects of computational thinking (CT) could be supported by educational contexts such as simulations and video-games construction. In this field study,...","categories": [],
+        "tags": [],
+        "url": "http://scholar.epidro.me/index.html",
+        "teaser":null},{
+        "title": "Publications",
+        "excerpt":"Recent Garneli, V. and Chorianopoulos, K. 2018. Programming video games and simulations in science education: exploring computational thinking through code analysis. Interactive Learning Environments 26, 3, 386–401. Various aspects of computational thinking (CT) could be supported by educational contexts such as simulations and video-games construction. In this field study, potential...","categories": [],
+        "tags": [],
+        "url": "http://scholar.epidro.me/publications/",
+        "teaser":null},{
+        "title": "Resume",
+        "excerpt":"Resume Summary I am doing research and teaching in the broad area of Informatics with an emphasis on the domains of user interfaces, video, video-games, gamification, crowdsourcing, software, learning, health. Research methodology: human-centered software design, user study (field, experiment). Application domains: Entertainment, learning, well-being, culture. Recent work During the last...","categories": [],
+        "tags": [],
+        "url": "http://scholar.epidro.me/resume/",
+        "teaser":null},]
