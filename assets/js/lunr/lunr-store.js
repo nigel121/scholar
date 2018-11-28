4cf4cfe91d9a368e242c0cfa7c8b2174234cd363
epidrome <github@epidro.me>
2018-11-29 01:32:41 +0200

    updated

--- assets/js/lunr/lunr-store.js
+++ assets/js/lunr/lunr-store.js
@@ -1,31 +1,31 @@
 var store = [{
         "title": "2014 09 Icec Chair",
-        "excerpt":"General co-chair of the IFIP International Conference on Entertainment Computing 2015 ","categories": [],
+        "excerpt":"General co-chair of the IFIP International Conference on Entertainment Computing 2015  ","categories": [],
         "tags": [],
         "url": "http://scholar.epidro.me/news/2014-09-icec-chair/",
         "teaser":null},{
         "title": "2016 09 Pibook",
-        "excerpt":"Self-published a book on making of interactive systems! 📖 ","categories": [],
+        "excerpt":"Self-published a book on making of interactive systems! 📖  ","categories": [],
         "tags": [],
         "url": "http://scholar.epidro.me/news/2016-09-pibook/",
         "teaser":null},{
         "title": "2017 04 Percom Publicity",
-        "excerpt":"Accepted invitation to serve as publicity co-chair for the IEEE Pervasive Computing 2018 conference 📢 ","categories": [],
+        "excerpt":"Accepted invitation to serve as publicity co-chair for the IEEE Pervasive Computing 2018 conference 📢  ","categories": [],
         "tags": [],
         "url": "http://scholar.epidro.me/news/2017-04-percom-publicity/",
         "teaser":null},{
         "title": "2017 06 Varvara Phd",
-        "excerpt":"Post-graduate student Varvara Garneli successfully defended PhD thesis 🎓 ","categories": [],
+        "excerpt":"Post-graduate student Varvara Garneli successfully defended PhD thesis 🎓  ","categories": [],
         "tags": [],
         "url": "http://scholar.epidro.me/news/2017-06-varvara-phd/",
         "teaser":null},{
         "title": "2017 09 Msc Ubicomp",
-        "excerpt":"Selected to teach at the MSc on Mobile and Ubiquitous Computing at the Hellenic Open University ","categories": [],
+        "excerpt":"Selected to teach at the MSc on Mobile and Ubiquitous Computing at the Hellenic Open University  ","categories": [],
         "tags": [],
         "url": "http://scholar.epidro.me/news/2017-09-msc-ubicomp/",
         "teaser":null},{
         "title": "2018 03 Tenured",
-        "excerpt":"Got tenured at the rank of assistant professor! :fireworks: ","categories": [],
+        "excerpt":"Got tenured at the rank of assistant professor! :fireworks:  ","categories": [],
         "tags": [],
         "url": "http://scholar.epidro.me/news/2018-03-tenured/",
         "teaser":null},]
